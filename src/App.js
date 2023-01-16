2e711b97b35d79d6a004983f0f7b19b53a63696c
John Prado <jcprado20@icloud.com>
2023-01-15 19:44:40 -0500

    add fetched data into student list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {useEffect, useState} from 'react';
+import StudentCardList from './components/StudentCardList/StudentCardList';
+
+const API = process.env.REACT_APP_API_URL;
 
 function App() {
+
+const [ students, setStudents ] = useState([]);
+
+useEffect(() => {
+
+  fetch(`${API}/students`)
+  .then(res => res.json())
+  .then(data => setStudents(data))
+}, [])
+
   return (
    <div>
-    <h1>Hello World</h1>
+    <StudentCardList students={students}/>
    </div>
   );
 }
--- src/components/StudentCard/StudentCard.js
+++ src/components/StudentCard/StudentCard.js
@@ -2,9 +2,31 @@ import './StudentCard.css'
 
 import React from 'react'
 
-function StudentCard() {
+function StudentCard({ student }) {
+const numericGrades = student.grades.map((grade) => Number(grade));
+let total = 0;
+for (const grade of numericGrades) {
+    total += grade;
+  }
+const average = total / numericGrades.length;
+
   return (
-    <div>StudentCard</div>
+    <div className="StudentCard" key={student.id}>
+      <div className="StudentCard__avatar">
+        <img src={student.pic} alt={`${student.firstName} ${student.lastName}`} />
+      </div>
+      <div className="StudentCard__info">
+        <h1>
+          {student.firstName} {student.lastName}
+        </h1>
+        <ul>
+          <li>Email: {student.email}</li>
+          <li>Company: {student.company} </li>
+          <li>Skill: {student.skill}</li>
+          <li>Average: {average}%</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
